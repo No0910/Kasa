7638c1e162a5dfdb9621a7e5d3d69af479fdaada
No0910 <nestea_93@hotmail.fr>
2023-01-31 17:15:41 +0100

    Footer page d'accueil Desktop

--- kasa_p7/src/components/Footer/index.jsx
+++ kasa_p7/src/components/Footer/index.jsx
@@ -1,7 +1,11 @@
+import "../../components/Footer/index.css";
+import LogoFooter from "../../assets/Footer-logo.png"
+
 function Footer() {
     return (
-        <div>
-            <h1></h1>
+        <div className = "Footer-div">
+            <img className="Footer-logo" src={LogoFooter} alt="Logo" />
+            <h1 className = "Footer-h1">© 2020 Kasa. All rights reserved</h1>
         </div>
     )
 }
