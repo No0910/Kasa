057030822ec88af0fabc5110612ed8e657a148c4
No0910 <nestea_93@hotmail.fr>
2023-02-03 21:06:50 +0100

    Page Erreur 404: Partie jsx OK, manque le CSS

--- kasa_p7/src/pages/Error/index.jsx
+++ kasa_p7/src/pages/Error/index.jsx
@@ -1,9 +1,15 @@
 // Création de la page Erreur 404
+import { Link } from 'react-router-dom'
+import Logo_Error from "../../assets/Logo_Error_404.png"
+import "./index.css"
+
 
 function Error() {
     return (
-        <div>
+        <div className="Error_div">
+            <img className="Logo_Error_404" src={Logo_Error} alt="Erreur 404"/>
             <h1>Oups! La page que vous demandez n'existe pas.</h1>
+            <Link to="/">Retourner sur la page d'accueil</Link>
         </div>
     )
 }
