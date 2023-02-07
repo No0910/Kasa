b61155cc03f3ffedcc9aa8bd631f9fdb6bbe9346
No0910 <nestea_93@hotmail.fr>
2023-02-07 20:08:49 +0100

    Css de la page logement en cours

--- kasa_p7/src/pages/Logement/index.jsx
+++ kasa_p7/src/pages/Logement/index.jsx
@@ -11,7 +11,7 @@ function Logement() {
   //
   const { idLogement } = useParams()
 
-  //
+  // Condition si l'id du logement ne correspond pas
   if (!idLogement) {
     return (
       <>
@@ -20,47 +20,51 @@ function Logement() {
     )
   }
 
+  // Récupération des logements avec la méthode filter() + la méthode shift()
   const listingArray = logements.filter((el) => el.id === idLogement)
   const logement = listingArray.shift()
+
+  // Récupération des notations logement avec étoiles
+  const ratingStars = logement.rating;
+
   //const totalStars = 5;
   //const selectedStars = Number(logement.rating);
-  const ratingStars = logement.rating;
 
   return (
     <main className="Logement_main">
       <div className="div_logement">
+
             <div className="logement_content">
 
-                    <div className="logement_content_infos">
-                        <h1 className="logement_title"> {logement.title} </h1>
-                        <p className="logement_location"> {logement.location} </p>
-                        <div className="logement_tag">
-                            {logement.tags.map((tag, index) => {
-                                return (
-                                    <button key={index}>{tag}</button>
-                                )
-                            })}
-                        </div> 
-                    </div> 
-            
+                  <div className="logement_content_infos">
+                      <h1 className="logement_title"> {logement.title} </h1>
+                      <p className="logement_location"> {logement.location} </p>
+                      <div className="logement_tag">
+                          {logement.tags.map((tag, index) => {
+                              return (
+                                  <button key={index}>{tag}</button>
+                              )
+                          })}
+                      </div> 
+                  </div> 
+          
+                  <div className="logement_content_host">
+                
+                      <div className="logement_content_host_name">
+                          <p>{logement.host.name}</p>
+                          <img src={logement.host.picture} alt="Hôte du logement"/>
+                      </div>
 
-                    <div className="logement_content_host">
-                 
-                        <div className="logement_content_host_name">
-                            <p>{logement.host.name}</p>
-							<img src={logement.host.picture} alt="Hôte du logement" />
-						</div>
+                      <div className="logement_content_host_stars">
+                          {[...Array(5)].map((star, index) => {
+                            const ratingValue = index + 1;
+                            return (
+                              <img key={index} src={ratingValue <= ratingStars ? redStar : greyStar} alt="star" />
+                            )
+                          })}
+                      </div>
 
-                        <div className="logement_content_host_stars">
-							{[...Array(5)].map((star, index) => {
-								const ratingValue = index + 1;
-								return (
-									<img key={index} src={ratingValue <= ratingStars ? redStar : greyStar} alt="star" />
-								)
-							})}
-						</div>
-
-                    </div>   
+                  </div>   
             </div>
 
                 <div className="logement_collapse">
